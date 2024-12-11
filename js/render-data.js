68d766047b245c70cb32dbe903321d289da8899c
Pavel Kuznetsov <pavel.kuznetsov.alexandrovich@gmail.com>
2024-12-11 11:45:39 +0300

    add-filters

--- js/render-data.js
+++ js/render-data.js
@@ -3,12 +3,14 @@
 import {showDataError} from './util.js';
 import {getData} from './api.js';
 import {renderThumbnails} from './render-thumbnails.js';
+import {renderFilteredThumbnails} from './filter-thumbnails.js';
 
 // Функция загружает и рендерит все данные на странице
 const renderData = () => {
   getData()
     .then((pictures) => {
       renderThumbnails(pictures);
+      renderFilteredThumbnails(pictures);
     })
     .catch(() => showDataError());
 };
--- js/util.js
+++ js/util.js
@@ -1,6 +1,28 @@
 // Модуль содержит утилитарные функции
 
-import {DATA_ERROR_MESSAGE_TIME} from './const.js';
+import {DATA_ERROR_MESSAGE_TIME, RERENDER_DELAY} from './const.js';
+
+// Функция возвращает случайное число из диапазона от min до max включительно
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(min, max));
+  const upper = Math.floor(Math.max(min, max));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+// Функция возвращает массив со случайными уникальными элементами
+const getRandomUniqArray = (array) => {
+  const arrayCopy = array.slice();
+  const uniqArray = [];
+
+  for (let i = 0; i < arrayCopy.length; i++) {
+    const randomId = getRandomInteger(0, arrayCopy.length - 1);
+    uniqArray.push(arrayCopy[randomId]);
+    arrayCopy.splice(randomId, 1);
+  }
+
+  return uniqArray;
+};
 
 // Функция показывает сообщение об ошибке загрузки данных
 const showDataError = () => {
@@ -18,38 +40,18 @@ const showDataError = () => {
 // Функция проверяет нажата ли клавиша Escape
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
-// // Функция игнорирует нажатие Escape когда в фокусе текстовое поле
+// Функция игнорирует нажатие Escape когда в фокусе текстовое поле
 const onTextInputFocusKeydown = (evt) => {
   evt.stopPropagation();
 };
 
-export {showDataError, isEscapeKey, onTextInputFocusKeydown};
-
-/*
-later
-
-// Функция возвращает случайное число из диапазона от min до max включительно
-const getRandomInteger = (min, max) => {
-  const lower = Math.ceil(Math.min(min, max));
-  const upper = Math.floor(Math.max(min, max));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
-
-// Функция возвращает случайное число из диапазона от min до max включительно так, что число не повторяется, пока не будут перебраны все числа из диапазона
-const createRandomIntegerFromRangeGenerator = (min, max) => {
-  const previousValues = [];
-
-  return () => {
-    let currentValue = getRandomInteger(min, max);
-    if (previousValues.length >= (max - min + 1)) {
-      return null;
-    }
-    while (previousValues.includes(currentValue)) {
-      currentValue = getRandomInteger(min, max);
-    }
-    previousValues.push(currentValue);
-    return currentValue;
+// Функция устранения дребезга
+const debounce = (callback, timeoutDelay = RERENDER_DELAY) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
   };
 };
-*/
+
+export {getRandomUniqArray, showDataError, isEscapeKey, onTextInputFocusKeydown, debounce};
